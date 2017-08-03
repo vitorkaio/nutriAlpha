d6094bee9c8ddc73793e11f2e60137d2935a316b
Vítor <vitorcaiodepaula@gmail.com>
2017-08-03 17:08:12 -0300

    Somatório das gramas
    Adição das operações de soma dos alimentos

--- src/pages/cesta-alimento/cesta-alimento.ts
+++ src/pages/cesta-alimento/cesta-alimento.ts
@@ -1,7 +1,7 @@
 import { Soma } from './../../model/soma.model';
 import { Alimento } from './../../model/alimento.model';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the CestaAlimentoPage page.
@@ -18,41 +18,93 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
 export class CestaAlimentoPage {
 
     private cesta: Alimento[] = this.navParams.get('cesta');
+    private quantidade: any;
     private soma: Alimento;
     private sum: Soma;
 
-    constructor(public navCtrl: NavController, public navParams: NavParams, public view: ViewController) {
+    constructor(public navCtrl: NavController, public navParams: NavParams, public view: ViewController,
+    public alertCtrl: AlertController) {
+
+        this.quantidade = [];
+        for(let i = 0; i < this.cesta.length; i++){
+            this.quantidade.push({'quantidade': 100, alimento: this.cesta[i].nome});
+        }
+
         this.somaCesta();
     }
 
     ionViewDidLoad() {
-        console.log(this.cesta);
+        //console.log(this.cesta);
     }
 
     ionViewDidLeave(){
-        console.log("close modal page cesta");
+        //console.log("close modal page cesta");
     }
 
     // Soma as propriedades da cesta
     public somaCesta(){
-        console.log('Soma!');
+        //console.log('Soma!');
         this.sum = {};
         this.sum.kcal = 0;
         this.cesta.forEach(element => {
             this.sum.kcal += Number(element.kcal);
-            console.log(this.sum);
+            //console.log(this.sum);
         });
 
     }
 
+    // Reseta a cesta de alimentos.
     public resetaCesta(){
         this.cesta = new Array();
-        console.log(this.cesta);
+        //console.log(this.cesta);
         this.view.dismiss(true);
     }
 
     public clicaAlimento(alimento: Alimento){
-        console.log(alimento.nome);
+
+        let gramas: number = 0;
+        for(let i = 0; i < this.quantidade.length; i++){
+            console.log('Food: ' + this.quantidade[i]['alimento']);
+            if (this.quantidade[i]['alimento'] == alimento.nome)
+                gramas = this.quantidade[i]['quantidade'];
+        }
+
+        let prompt = this.alertCtrl.create({
+            title: 'Quantidade',
+            message: "Entre com a quantidade em gramas, atual: " + gramas,
+            inputs: [
+                {name: 'title', placeholder: 'Title'},
+            ],
+            buttons: [
+                {text: 'Cancel', handler: data => {
+                        console.log('Cancel clicked');
+                    }
+                },
+                {text: 'Save',
+                    handler: data => {
+                        console.log('Saved clicked');
+                    }
+                }
+            ]
+        });
+        prompt.present();
     }
 
-}
+    // Remove um alimento da cesta.
+    public removeAlimento(alimento: Alimento){
+        for(let i = 0; i < this.cesta.length; i++){
+            if (this.cesta[i].nome == alimento.nome){
+                this.cesta.splice(i, 1);
+                break;
+            }
+        }
+
+        // Verifica se a cesta está vazia.
+        if(this.cesta.length == 0)
+            this.resetaCesta();
+
+        // Refaz a soma.
+        this.somaCesta();
+    }
+
+}// Fim da classe cestaAlimentos
